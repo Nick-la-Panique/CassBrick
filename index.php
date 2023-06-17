2886d39f2b13965a68fa438ac6ff94173bb16562
Nicolas BLOT <135271290+Nick-la-Panique@users.noreply.github.com>
2023-06-17 14:57:05 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>CassBrick</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="assets/script.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
